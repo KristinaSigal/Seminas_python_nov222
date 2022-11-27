aa7c728a15f06e74a6d0683e8a30e83c01f215d6
kristina.sigal <kulrikh@gmail.com>
2022-11-27 23:57:21 +0300

    Edit task 2

--- task_2.py
+++ task_2.py
@@ -4,16 +4,12 @@
 # Ввод: любая коллекция значений типа <int>
 # Вывод: значение типа <int>
 
-a = int(input('Введите число a '))
-b = int(input('Введите число b '))
-c = int(input('Введите число c '))
-d = int(input('Введите число d '))
-e = int(input('Введите число e '))
-
-some_list = [a, b, c, d, e]
-print(some_list)
+some_list = []
+for i in 1, 2, 3, 4, 5:
+    some_list.append(int(input(f'введите {i} значение: ')))
+print (some_list)
 max = some_list[0]
 for i in some_list:
-    if max < i: 
-        max = i
-print(max)
+    if max < i:
+        max=i
+print(f'максимальное число {max}')
