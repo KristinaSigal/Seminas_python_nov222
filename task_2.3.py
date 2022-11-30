a66999e2a9cb5d346739fa8f4bf5aa0e1090a74c
kristina.sigal <kulrikh@gmail.com>
2022-12-01 01:45:42 +0300

    Resave task 2.3

--- task_2.3.py
+++ task_2.3.py
@@ -14,17 +14,35 @@ o
 2
 """
 
-str_one = 'good news everyone, or one'
-str_two = 'one'
-#S.find(str, [start], [end])
-count = 0
-index = 0
-start = index
-while index != -1:
-    if str_one.find(str_two, start+1) != -1:
-        index = str_one.find(str_two, start+1)
-        start = index
-        count += 1
-    else:
-        index = -1
-print(count)
+str1 = input("Введите первую сторку: ")
+str2 = input("Введите вторую сторку: ")
+
+print(str1.count(str2))
+
+# #Вариант Данила 
+# str1 = input("Введите первую сторку: ")
+# str2 = input("Введите вторую сторку: ")
+
+# inkrement = 0
+# for i in range(len(str1)):
+#     if str2 in str1[i:]:
+#         inkrement += 1
+# print(inkrement)
+
+# print(str1.count(str2))
+
+# #Мое решение
+# str_one = 'good news everyone, or one'
+# str_two = 'one'
+# #S.find(str, [start], [end])
+# count = 0
+# index = 0
+# start = index
+# while index != -1:
+#     if str_one.find(str_two, start+1) != -1:
+#         index = str_one.find(str_two, start+1)
+#         start = index
+#         count += 1
+#     else:
+#         index = -1
+# print(count)
